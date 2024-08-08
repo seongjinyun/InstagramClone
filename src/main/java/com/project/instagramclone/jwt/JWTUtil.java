a7962147d4e38a46c8ed7a0df1b724b98774a790
[SeongJin Yun] <[ckwdktek12@gmail.com]>
2024-08-08 20:04:04 +0900

    no message

--- src/main/java/com/project/instagramclone/jwt/JWTUtil.java
+++ src/main/java/com/project/instagramclone/jwt/JWTUtil.java
@@ -1,50 +1,41 @@
 package com.project.instagramclone.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.security.Key;
-import java.util.Date;
 
 @Component
 public class JWTUtil {
-
-    private Key key;
-
-    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
-
-
-        byte[] byteSecretKey = Decoders.BASE64.decode(secret);
-        key = Keys.hmacShaKeyFor(byteSecretKey);
-    }
-
-    
-    //nickname으로 사용중 수정해야 함
-    public String getUsername(String token) {
-
-        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().get("username", String.class);
-    }
-
-    public Boolean isExpired(String token) {
-
-        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getExpiration().before(new Date());
-    }
-
-    public String createJwt(String username, String role, Long expiredMs) {
-
-        Claims claims = Jwts.claims();
-        claims.put("username", username);
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-    }
+//
+//    private Key key;
+//
+//    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
+//
+//
+//        byte[] byteSecretKey = Decoders.BASE64.decode(secret);
+//        key = Keys.hmacShaKeyFor(byteSecretKey);
+//    }
+//
+//
+//    //nickname으로 사용중 수정해야 함
+//    public String getUsername(String token) {
+//
+//        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().get("username", String.class);
+//    }
+//
+//    public Boolean isExpired(String token) {
+//
+//        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getExpiration().before(new Date());
+//    }
+//
+//    public String createJwt(String username, String role, Long expiredMs) {
+//
+//        Claims claims = Jwts.claims();
+//        claims.put("username", username);
+//
+//        return Jwts.builder()
+//                .setClaims(claims)
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
+//                .signWith(key, SignatureAlgorithm.HS256)
+//                .compact();
+//    }
 }
